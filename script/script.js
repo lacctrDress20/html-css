90557b4901b0bde27c5ccc8a59e9c7941cfd4dfe
lacctrDress20 <1490244@senati.pe>
2023-04-14 11:26:40 -0500

    Menu 2,3

---
+++ script/script.js
@@ -0,0 +1,11 @@
+const body = document.querySelector("body"),
+      sidebar = body.querySelector(".sidebar"),
+      toggle = body.querySelector(".toggle"),
+      searchBtn = body.querySelector(".search-box"),
+      modeSwtich = body.querySelector(".mode"),
+      modeText = body.querySelector(".mode-text");
+
+      modeSwtich.addEventListener("click", () =>{
+        body.classList.toggle("dark");
+      });
+
